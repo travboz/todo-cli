485965617e9ba7dbc7d3ac36ee3d824751991826
Travis Bozic <z5368318@ad.unsw.edu.au>
2024-04-03 17:07:30 +1100

    Add new() constructor which reads in any previously created todos from our store.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::collections::HashMap;
 use std::env;
+use std::io::Read;
+use std::str::FromStr;
 
 fn main() {
     let action = env::args().nth(1).expect("Please specify an action");
     let item = env::args().nth(2).expect("Please specify an item");
 
     println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
+    if action == "add" {
+        todo.insert(item);
+
+        match todo.save() {
+            Ok(_) => println!("Todo has been saved."),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
 
 struct Todo {
@@ -13,12 +26,33 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         // insert a new item into the map
         // default value is true
         self.map.insert(key, true);
     }
 
+    // save takes ownership of self, so this enforces
+    // us to use save as the last method of a session.
     fn save(self) -> Result<(), std::io::Error> {
         let mut content = String::new();
         for (k, v) in self.map {
